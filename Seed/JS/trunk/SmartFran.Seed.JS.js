bebea0372cc4887bbca480a32083853d7187b10f
Jorge Pinetta <JorgeP@smartfran.com>
2012-10-10 17:43:50 +0000

    startup

---
+++ Seed/JS/trunk/SmartFran.Seed.JS.js
@@ -0,0 +1,13 @@
+﻿function namespace(namespace) {
+  var object = this, tokens = namespace.split("."), token;
+
+  while (tokens.length > 0) {
+    token = tokens.shift();
+
+    if (typeof object[token] === "undefined") {
+      object[token] = {};
+    }
+    object = object[token];
+  }
+  return object;
+}
