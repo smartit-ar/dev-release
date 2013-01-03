e3555df51212c21d45e796d67d59f4571b17dd15
Jorge Pinetta <JorgeP@smartfran.com>
2013-01-03 22:25:10 +0000

    * ticket appm-36

--- Seed/JS/trunk/SmartFran.Seed.JS.ko.Formatting.js
+++ Seed/JS/trunk/SmartFran.Seed.JS.ko.Formatting.js
@@ -47,4 +47,50 @@
 
     return result;
   };
+
+
+  var formatDecimal = function (value, decs) {
+    var toks = value.toFixed(decs).replace('-', '').split('.');
+    var display = $.map(toks[0].split('').reverse(), function (elm, i) {
+      return [(i % 3 === 0 && i > 0 ? ',' : ''), elm];
+    }).reverse().join('') + '.' + toks[1];
+
+    return value < 0 ? '-' + display : display;
+  };
+
+  ko.subscribable.fn.decimal = function (decs) {
+    var target = this;
+
+    var writeTarget = function (value) {
+      if (typeof value == 'number') {
+        target(value);
+      }
+      else {
+        var stripped = value
+          .replace(/[^0-9.-]/g, '');
+
+        target(parseFloat(stripped));
+      }
+    };
+
+    var result = ko.computed({
+      read: function () {
+        return target();
+      },
+      write: writeTarget
+    });
+
+    result.formatted = ko.computed({
+      read: function () {
+        return formatDecimal(target(), decs);
+      },
+      write: writeTarget
+    });
+
+    result.isNegative = ko.computed(function () {
+      return target() < 0;
+    });
+
+    return result;
+  };
 })();
