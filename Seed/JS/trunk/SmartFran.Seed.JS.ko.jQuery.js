af5ca46e8a35e28f85041c290afdd3234979994e
Jorge Pinetta <JorgeP@smartfran.com>
2013-05-03 20:21:06 +0000

    DatePicker

--- Seed/JS/trunk/SmartFran.Seed.JS.ko.jQuery.js
+++ Seed/JS/trunk/SmartFran.Seed.JS.ko.jQuery.js
@@ -17,7 +17,6 @@
 //   <button data-bind="buttonEdit: {}, click: $root.cancel">Cancel</button>
 // </script>
 //
-
 (namespace('SmartFran.Seed.JS.ko').jQuery = function() {
   //custom binding to initialize a jQuery UI dialog
   ko.bindingHandlers.jqDialog = {
@@ -56,6 +55,50 @@
       });
 
       $(element).button(options);
+    }
+  };
+  
+  //custom binding handler for datepicker
+  ko.bindingHandlers.datepicker = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+      var options = allBindingsAccessor().datepickerOptions || {};
+
+      $(element)
+        .datepicker(options)
+        .bind("change", function () {
+          ko.bindingHandlers.datepicker.updateValue(element, valueAccessor, allBindingsAccessor);
+        });
+
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        $(element).datepicker("destroy");
+      });
+    },
+    update: function (element, valueAccessor, allBindingsAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor());
+
+      // If the date is coming from a Microsoft webservice.
+      if (typeof value === "string" && value.indexOf('/Date(') === 0) {
+        value = new Date(parseInt(value.replace(/\/Date\((.*?)\)\//gi, "$1")));
+      }
+      var currentDate = $(element).datepicker("getDate");
+
+      // Check if the date has changed.
+      if (value && value - currentDate !== 0) {
+        $(element).datepicker("setDate", value);
+      }
+    },
+    updateValue: function (element, valueAccessor, allBindingsAccessor) {
+      var observable = valueAccessor(),
+          dateValue = $(element).datepicker("getDate");
+
+      // Two-way-binding means a writeable observable.
+      if (ko.isWriteableObservable(observable)) {
+        observable(dateValue);
+        return;
+      }
+      if (allBindingsAccessor()._ko_property_writers) {
+        allBindingsAccessor()._ko_property_writers.datepicker(dateValue);
+      }
     }
   };
 })();
