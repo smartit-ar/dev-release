4a8172af1d0bbbfc1c56428a512fa5ef15ecacc4
Jorge Pinetta <JorgeP@smartfran.com>
2013-05-16 13:57:35 +0000

    * Rename y agregado del spinner

---
+++ Seed/JS/trunk/SmartFran.Seed.JS.ko.PluginUI.js
@@ -0,0 +1,136 @@
+﻿(namespace('SmartFran.Seed.JS.ko').PluginUI = function () {
+  //custom binding to initialize a jQuery UI dialog
+  ko.bindingHandlers.jqDialog = {
+    init: function (element, valueAccessor) {
+      var options = ko.utils.unwrapObservable(valueAccessor()) || {};
+
+      //handle disposal
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        $(element).dialog("destroy");
+      });
+
+      $(element).dialog(options);
+    }
+  };
+
+  //custom binding handler that opens/closes the dialog
+  ko.bindingHandlers.openDialog = {
+    update: function (element, valueAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor());
+      if (value) {
+        $(element).dialog("open");
+      } else {
+        $(element).dialog("close");
+      }
+    }
+  };
+  
+  ko.bindingHandlers.spinner = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+      //initialize datepicker with some optional options
+      var options = allBindingsAccessor().spinnerOptions || {};
+      $(element).spinner(options);
+
+      //handle the field changing
+      ko.utils.registerEventHandler(element, "spinchange", function () {
+        var observable = valueAccessor();
+        observable($(element).spinner("value"));
+      });
+
+      //handle disposal (if KO removes by the template binding)
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        $(element).spinner("destroy");
+      });
+
+    },
+    update: function (element, valueAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor());
+
+      var current = $(element).spinner("value");
+      if (value !== current) {
+        $(element).spinner("value", value);
+      }
+    }
+  };
+
+  //custom binding to initialize a jQuery UI button
+  ko.bindingHandlers.jqButton = {
+    init: function (element, valueAccessor) {
+      var options = ko.utils.unwrapObservable(valueAccessor()) || {};
+
+      //handle disposal
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        $(element).button("destroy");
+      });
+
+      $(element).button(options);
+    }
+  };
+
+  //custom binding handler for datepicker
+  ko.bindingHandlers.datepicker = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+      //Calcula rango de años a mostrar cuando option 'changeYear:true'
+      function getDateRange() {
+        var d = new Date();
+        var dateRange = (d.getFullYear() - 100) + ':' + (d.getFullYear() + 10);
+        return dateRange;
+      }
+      var options = allBindingsAccessor().datepickerOptions;
+      options.monthNames = options.monthNames || ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+      options.monthNamesShort = options.monthNamesShort || ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+      options.dateFormat = options.dateFormat || 'dd/mm/yy';
+      options.yearRange = options.yearRange || getDateRange();
+
+      $(element)
+        .datepicker(options)
+        .bind("change", function () {
+          ko.bindingHandlers.datepicker.updateValue(element, valueAccessor, allBindingsAccessor);
+        });
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        $(element).datepicker("destroy");
+      });
+    },
+    update: function (element, valueAccessor, allBindingsAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor());
+
+      // If the date is coming from a Microsoft webservice.
+      if (typeof value === "string" && value.indexOf('/Date(') === 0) {
+        value = new Date(parseInt(value.replace(/\/Date\((.*?)\)\//gi, "$1")));
+      }
+      var currentDate = $(element).datepicker("getDate");
+
+      // Check if the date has changed.
+      if (value && value - currentDate !== 0) {
+        $(element).datepicker("setDate", value);
+      }
+    },
+    updateValue: function (element, valueAccessor, allBindingsAccessor) {
+      var observable = valueAccessor(),
+        dateValue = $(element).datepicker("getDate");
+
+      // Two-way-binding means a writeable observable.
+      if (ko.isWriteableObservable(observable)) {
+        observable(dateValue);
+        return;
+      }
+      if (allBindingsAccessor()._ko_property_writers) {
+        allBindingsAccessor()._ko_property_writers.datepicker(dateValue);
+      }
+    }
+  };
+  ko.bindingHandlers.masked = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+      var mask = allBindingsAccessor().mask || {};
+      $(element).mask(mask);
+      ko.utils.registerEventHandler(element, 'focusout', function () {
+        var observable = valueAccessor();
+        observable($(element).val());
+      });
+    },
+    update: function (element, valueAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor());
+      $(element).val(value);
+    }
+  };
+})();
