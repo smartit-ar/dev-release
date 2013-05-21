58b52af0f52ed551ddba5aecec90a5930e5c8712
Jorge Pinetta <JorgeP@smartfran.com>
2013-05-21 12:27:26 +0000

    * se agregan controles

---
+++ Seed/JS/trunk/SmartFran.Seed.JS.ko.PluginUI.jqDialog.js
@@ -0,0 +1,26 @@
+﻿(namespace('SmartFran.Seed.JS.ko.PluginUI').jqDialog = function () {
+  ko.bindingHandlers.jqDialog = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+      var options = ko.utils.unwrapObservable(valueAccessor()) || {};
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        $(element).dialog("destroy");
+      });
+      options.close = function () {
+        allBindingsAccessor().openDialog(false);
+      };
+      $(element).dialog(options);
+    }
+  };
+
+  ko.bindingHandlers.jqOpenDialog = {
+    update: function (element, valueAccessor) {
+      var value = ko.utils.unwrapObservable(valueAccessor());
+      if (value) {
+        $(element).dialog("open");
+      } else {
+        $(element).dialog("close");
+      }
+    }
+  };
+
+})();
--- Seed/JS/trunk/SmartFran.Seed.JS.ko.PluginUI.js
+++ Seed/JS/trunk/SmartFran.Seed.JS.ko.PluginUI.js
@@ -20,30 +20,6 @@
       $(element).val(value);
     }
   };
-  
-  ko.bindingHandlers.jqDialog = {
-    init: function (element, valueAccessor, allBindingsAccessor) {
-      var options = ko.utils.unwrapObservable(valueAccessor()) || {};
-      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-        $(element).dialog("destroy");
-      });
-      options.close = function () {
-        allBindingsAccessor().openDialog(false);
-      };
-      $(element).dialog(options);
-    }
-  };
-
-  ko.bindingHandlers.jqOpenDialog = {
-    update: function (element, valueAccessor) {
-      var value = ko.utils.unwrapObservable(valueAccessor());
-      if (value) {
-        $(element).dialog("open");
-      } else {
-        $(element).dialog("close");
-      }
-    }
-  };
 
   ko.bindingHandlers.jqSpinner = {
     init: function (element, valueAccessor, allBindingsAccessor) {
