1d85b931504eb1b7088762fdadca578ab63f9ad6
Jorge Pinetta <JorgeP@smartfran.com>
2013-09-27 18:50:02 +0000

    Corrije la deteccion del ultimo dia del mes-.

--- Seed/JS/trunk/SmartFran.Seed.JS.Utility.js
+++ Seed/JS/trunk/SmartFran.Seed.JS.Utility.js
@@ -89,8 +89,8 @@
         month = parseInt(month);
         year = parseInt(year);
         switch (month) {
-          case 1: case 3: case 5: case 7: case 8: case 10: case 12: return 31;
-          case 2: return (year % 4 == 0) ? 29 : 28;
+          case 0: case 2: case 4: case 6: case 7: case 9: case 11: return 31;
+          case 1: return (year % 4 == 0) ? 29 : 28;
         }
         return 30;
       };
