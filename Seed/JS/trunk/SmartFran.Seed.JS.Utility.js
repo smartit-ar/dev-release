9af8bef2a82fd6816c9a048e696788e80be5b2e9
Jorge Pinetta <JorgeP@smartfran.com>
2019-05-13 11:22:06 +0000

    * AJUSTES EN FECHAS

--- Seed/JS/trunk/SmartFran.Seed.JS.Utility.js
+++ Seed/JS/trunk/SmartFran.Seed.JS.Utility.js
@@ -11,11 +11,11 @@ String.prototype.toCamelCase = function () {
 String.prototype.capitalizeFirstLetter = function () {
   var str = this.charAt(0).toUpperCase() + this.substring(1);
   return str.trim();
-}
-namespace('SmartFran.Seed.JS').Utility = {
+};
+namespace("SmartFran.Seed.JS").Utility = {
   Date: {
     ageToCertainDate: function (birthDate, certainDate) {
-      if ((birthDate == null || birthDate == 'undefined') || (certainDate == null || certainDate == 'undefined')) {
+      if ((birthDate == null || birthDate == "undefined") || (certainDate == null || certainDate == "undefined")) {
         return null;
       }
 
@@ -49,14 +49,14 @@ namespace('SmartFran.Seed.JS').Utility = {
       var diaMes = fecha.getDate();
       var diaSemana = fecha.getDay();
       var anio = fecha.getFullYear();
-      var dias = new Array('Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado');
-      var meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
+      var dias = new Array("Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado");
+      var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
       document.write(dias[diaSemana] + ", " + diaMes + " de " + meses[mes] + " de " + anio);
     },
     writeSpShortWeekDay: function () {
       var fecha = new Date();
       var diaSemana = fecha.getDay();
-      var dias = new Array('DOM', 'LUN', 'MAR', 'MIE', 'JUE', 'VIE', 'SAB');
+      var dias = new Array("DOM", "LUN", "MAR", "MIE", "JUE", "VIE", "SAB");
       document.write(dias[diaSemana]);
     },
     writeSpShortMonth: function (mes) {
@@ -64,7 +64,7 @@ namespace('SmartFran.Seed.JS').Utility = {
         var fecha = new Date();
         mes = fecha.getMonth();
       }
-      var meses = new Array('ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC');
+      var meses = new Array("ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEP", "OCT", "NOV", "DIC");
       document.write(meses[mes]);
       return meses[mes];
     },
@@ -73,7 +73,7 @@ namespace('SmartFran.Seed.JS').Utility = {
         var fecha = new Date();
         mes = fecha.getMonth();
       }
-      var meses = new Array('ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC');
+      var meses = new Array("ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEP", "OCT", "NOV", "DIC");
       return meses[mes];
     },
     writeDateMonthDay: function () {
@@ -89,14 +89,14 @@ namespace('SmartFran.Seed.JS').Utility = {
       document.write(anio);
     },
     dateTimeToDotNetTicks: function (date) {
-      if (date == null || date == 'undefined') {
+      if (date == null || date == "undefined") {
         return null;
       }
 
       return (date.getTime() * 10000) + 621355968000000000;
     },
     dateTimeToJson: function (date) {
-      if (date == null || date == 'undefined') {
+      if (date == null || date == "undefined") {
         return null;
       }
 
@@ -109,7 +109,7 @@ namespace('SmartFran.Seed.JS').Utility = {
       return "/Date(" + date + ")/";
     },
     dateTimeUtcToJson: function (date) {
-      if (date == null || date == 'undefined') {
+      if (date == null || date == "undefined") {
         return null;
       }
 
@@ -122,7 +122,7 @@ namespace('SmartFran.Seed.JS').Utility = {
       return "/Date(" + (parseInt(date) + ((new Date()).getTimezoneOffset() * 60000)) + ")/";
     },
     jsonToDateTime: function (date) {
-      if (date == null || date == 'undefined') {
+      if (date == null || date == "undefined") {
         return null;
       }
 
@@ -137,7 +137,7 @@ namespace('SmartFran.Seed.JS').Utility = {
       return new Date(date);
     },
     isJsonDateTime: function (date) {
-      if (date == null || date == 'undefined') {
+      if (date == null || date == "undefined") {
         return null;
       }
 
@@ -145,7 +145,7 @@ namespace('SmartFran.Seed.JS').Utility = {
       return regJsonDate.exec(date) != null;
     },
     getDateWithoutTime: function (date) {
-      if (date == null || date == 'undefined') {
+      if (date == null || date == "undefined") {
         return null;
       }
 
@@ -193,7 +193,7 @@ namespace('SmartFran.Seed.JS').Utility = {
       return new Date(date.getFullYear(), date.getMonth() - 1, getLastDay(date.getMonth(), date.getFullYear()));
     },
     totalDaysCalculator: function (since, to) {
-      if ((since == null || since == 'undefined') || (to == null || to == 'undefined')) {
+      if ((since == null || since == "undefined") || (to == null || to == "undefined")) {
         return null;
       }
 
@@ -203,39 +203,113 @@ namespace('SmartFran.Seed.JS').Utility = {
       }
       return Math.ceil(datediff / 86400000);
     },
+
     totalSelectedDaysCalculator: function (since, to) {
-      if ((since == null || since == 'undefined') || (to == null || to == 'undefined')) {
+      if ((since == null || since == "undefined") || (to == null || to == "undefined")) {
         return null;
       }
 
       var datediff = Seed.Utility.Date.jsonToDateTime(to) - Seed.Utility.Date.jsonToDateTime(since);
       return Math.ceil(datediff / 86400000) + 1;
     },
+
     getTodayPlusDays: function (days) {
-      if ((days == 'undefined') || (days == null) || (typeof days !== 'number')) {
+      if ((days == "undefined") || (days == null) || (typeof days !== "number")) {
         days = 0;
       }
 
-      return new Date().setDate(new Date().getDate() + days);
-    }
-  },
-  Number: {
-    getStringMaskedIntegerNumber: function (number) {
-      if (number == null || number == 'undefined') {
+      var date = new Date();
+      return date.setDate(date.getDate() + days);
+    },
+
+    add: function (part, offset, date) {
+      if (
+        typeof part !== "string"
+        || typeof offset !== "number"
+        || Object.prototype.toString.call(date) !== "[object Date]") {
         return null;
       }
 
-      var stringNumber = '' + number;
+      var result = date;
+      if (part.toLowerCase().substr(0, 3) == "day") {
+        result = new Date(date.setDate(date.getDate() + offset));
+      }
+      else if (part.toLowerCase().substr(0, 4) == "week") {
+        result = new Date(date.setDate(date.getDate() + (offset * 7)));
+      }
+      else if (part.toLowerCase().substr(0, 4) == "hour") {
+        result = new Date(date.setHours(date.getHours() + offset));
+      }
+      else if (part.toLowerCase().substr(0, 3) == "min") {
+        result = new Date(date.setMinutes(date.getMinutes() + offset));
+      }
+      else if (part.toLowerCase().substr(0, 3) == "sec") {
+        result = new Date(date.setSeconds(date.getSeconds() + offset));
+      }
+      else if (part.toLowerCase().substr(0, 5) == "month") {
+        result = new Date(date.setMonth(date.getMonth() + offset));
+      }
+      else if (part.toLowerCase().substr(0, 4) == "year") {
+        result = new Date(date.setFullYear(date.getFullYear() + offset));
+      }
+
+      return result;
+    },
+
+    diff: function (part, since, to) {
+      if (
+        typeof part !== "string"
+        || Object.prototype.toString.call(since) !== "[object Date]"
+        || Object.prototype.toString.call(to) !== "[object Date]") {
+        return null;
+      }
+
+      var result = 0;
+      var diffMs = to - since;
+
+      if (part.toLowerCase().substr(0, 3) == "day") {
+        result = parseInt(diffMs / (1000 * 60 * 60 * 24));
+      }
+      else if (part.toLowerCase().substr(0, 4) == "hour") {
+        result = parseInt(diffMs / (1000 * 60 * 60));
+      }
+      else if (part.toLowerCase().substr(0, 3) == "min") {
+        result = parseInt(diffMs / (1000 * 60));
+      }
+      else if (part.toLowerCase().substr(0, 3) == "sec") {
+        result = parseInt(diffMs / 1000);
+      }
+      else if (part.toLowerCase().substr(0, 4) == "week") {
+        result = parseInt(diffMs / (1000 * 60 * 60 * 24 * 7));
+      }
+      else if (part.toLowerCase().substr(0, 5) == "month") {
+        result = (to.getMonth() + (12 * to.getFullYear())) - (since.getMonth() + (12 * since.getFullYear()));
+      }
+      else if (part.toLowerCase().substr(0, 4) == "year") {
+        result = parseInt(((to.getMonth() + (12 * to.getFullYear())) - (since.getMonth() + (12 * since.getFullYear()))) / 12);
+      }
+
+      return result;
+    },
+  },
+
+  Number: {
+    getStringMaskedIntegerNumber: function (number) {
+      if (number == null || number == "undefined") {
+        return null;
+      }
+
+      var stringNumber = "" + number;
       var amount = stringNumber.split("").reverse();
       var output = "";
       for (var i = 0; i <= amount.length - 1; i++) {
         output = amount[i] + output;
-        if ((i + 1) % 3 == 0 && (amount.length - 1) !== i) output = '.' + output;
+        if ((i + 1) % 3 == 0 && (amount.length - 1) !== i) output = "." + output;
       }
       return output;
     },
     getStringMaskedDoubleNumber: function (number) {
-      if (number == null || number == 'undefined') {
+      if (number == null || number == "undefined") {
         return null;
       }
 
@@ -245,40 +319,40 @@ namespace('SmartFran.Seed.JS').Utility = {
       var output = "";
       for (var i = 0; i <= amount.length - 1; i++) {
         output = amount[i] + output;
-        if ((i + 1) % 3 == 0 && (amount.length - 1) !== i) output = '.' + output;
+        if ((i + 1) % 3 == 0 && (amount.length - 1) !== i) output = "." + output;
       }
 
       return output + "," + stringNumberArray[1];
     },
     addZeros: function (str, len) {
-      if (typeof str === 'number' || Number(str)) {
+      if (typeof str === "number" || Number(str)) {
         str = str.toString();
-        return (len - str.length > 0) ? new Array(len + 1 - str.length).join('0') + str : str;
+        return (len - str.length > 0) ? new Array(len + 1 - str.length).join("0") + str : str;
       } else {
-        for (var i = 0, spl = str.split(' '); i < spl.length; spl[i] = (Number(spl[i]) && spl[i].length < len) ? addZeros(spl[i], len) : spl[i], str = (i == spl.length - 1) ? spl.join(' ') : str, i++);
+        for (var i = 0, spl = str.split(" "); i < spl.length; spl[i] = (Number(spl[i]) && spl[i].length < len) ? addZeros(spl[i], len) : spl[i], str = (i == spl.length - 1) ? spl.join(" ") : str, i++);
         return str;
       }
-    }
+    },
   },
   Html: {
     encodeTilde: function (str) {
-      str = str.replace(/á/g, '&aacute;');
-      str = str.replace(/é/g, '&eacute;');
-      str = str.replace(/í/g, '&iacute;');
-      str = str.replace(/ó/g, '&oacute;');
-      str = str.replace(/ú/g, '&uacute;');
+      str = str.replace(/á/g, "&aacute;");
+      str = str.replace(/é/g, "&eacute;");
+      str = str.replace(/í/g, "&iacute;");
+      str = str.replace(/ó/g, "&oacute;");
+      str = str.replace(/ú/g, "&uacute;");
 
-      str = str.replace(/Á/g, '&Aacute;');
-      str = str.replace(/É/g, '&Eacute;');
-      str = str.replace(/Í/g, '&Iacute;');
-      str = str.replace(/Ó/g, '&Oacute;');
-      str = str.replace(/Ú/g, '&Uacute;');
+      str = str.replace(/Á/g, "&Aacute;");
+      str = str.replace(/É/g, "&Eacute;");
+      str = str.replace(/Í/g, "&Iacute;");
+      str = str.replace(/Ó/g, "&Oacute;");
+      str = str.replace(/Ú/g, "&Uacute;");
 
-      str = str.replace(/ñ/g, '&ntilde;');
-      str = str.replace(/Ñ/g, '&Ntilde;');
+      str = str.replace(/ñ/g, "&ntilde;");
+      str = str.replace(/Ñ/g, "&Ntilde;");
       return str;
-    }
-  }
+    },
+  },
 };
 
 ko.bindingHandlers.verticalSlideVisible = {
@@ -291,5 +365,5 @@ ko.bindingHandlers.verticalSlideVisible = {
     // Whenever the value subsequently changes, slowly fade the element in or out
     var value = valueAccessor();
     ko.unwrap(value) ? $(element).slideDown() : $(element).slideUp();
-  }
+  },
 };
--- Seed/JS/trunk/SmartFran.Seed.JS.ko.Mask.js
+++ Seed/JS/trunk/SmartFran.Seed.JS.ko.Mask.js
@@ -1,37 +1,37 @@
-﻿(namespace('SmartFran.Seed.JS.ko').Mask = function () {
+﻿(namespace("SmartFran.Seed.JS.ko").Mask = function () {
   ko.subscribable.fn.maskedField = function (initParams) {
     var self = this;
     var input;
     initParams = initParams || {};
     if (typeof initParams !== "object") {
-      initParams = { mask: initParams };
+      initParams = { mask: initParams, };
     }
-    var mask = initParams.mask || 'remove';
-    var unmask = initParams.unmask || '';
+    var mask = initParams.mask || "remove";
+    var unmask = initParams.unmask || "";
 
     function prepareMask() {
       if (!input) {
         input = $("<input type='text'>");
       }
-      if (mask === 'currency') {
-        input.inputmask('$ 999,999,999.99', { numericInput: true });
+      if (mask === "currency") {
+        input.inputmask("$ 999,999,999.99", { numericInput: true, });
       } else {
         input.inputmask(mask);
       }
     }
 
     function getMaskValue(value) {
-      if (mask === 'currency') {
+      if (mask === "currency") {
         return formatToCurrency(value);
       }
 
-      var toEval = value;
-      if (typeof value === "string" && value.startsWith("/Date(")) {
-        toEval = new RegExp("^/Date\\((-?\\d+)\\)/$").exec(value);
+      var dateEval = value;
+      if (typeof dateEval === "string" && dateEval.startsWith("/Date(")) {
+        dateEval = new RegExp("^/Date\\((-?\\d+)\\)/$").exec(dateEval);
       }
 
-      if (toEval !== null) {
-        return moment(toEval).format(mask);
+      if (dateEval !== null && Object.prototype.toString.call(dateEval) === "[object Date]") {
+        return moment(dateEval).format(mask);
       }
 
       prepareMask();
@@ -43,29 +43,29 @@
       if (value === null) {
         return null;
       }
-      var part = value.toString().split('.');
-      var dec = '00';
-      var inte = '0';
+      var part = value.toString().split(".");
+      var dec = "00";
+      var inte = "0";
       if (part.length === 2) {
         inte = part[0];
-        dec = (part[1] + '00').substr(0, 2);
+        dec = (part[1] + "00").substr(0, 2);
       }
       if (part.length === 1) {
         inte = part[0];
       }
-      return '$' + inte + '.' + dec;
+      return "$" + inte + "." + dec;
     };
 
     function getUnmaskValue(value) {
       if (unmask) {
-        return value.replace(unmask, '');
+        return value.replace(unmask, "");
       }
 
       prepareMask();
       input.val(value).change();
       var result = value;
-      if (input.inputmask('hasMaskedValue')) {
-        result = input.inputmask('unmaskedvalue');
+      if (input.inputmask("hasMaskedValue")) {
+        result = input.inputmask("unmaskedvalue");
       }
       return result;
     }
@@ -83,7 +83,7 @@
           self(unmaskVal);
         };
       },
-      owner: self
+      owner: self,
     });
 
     self.masked.assignMask = function (elem) {
@@ -93,16 +93,16 @@
     self.masked.setMask = function (params) {
       params = params || {};
       if (typeof params != "object") {
-        params = { mask: params };
+        params = { mask: params, };
       }
-      mask = params.mask || 'remove';
-      unmask = params.unmask || '';
+      mask = params.mask || "remove";
+      unmask = params.unmask || "";
       prepareMask();
     };
 
     self.masked.noMask = function () {
-      mask = 'remove';
-      unmask = '';
+      mask = "remove";
+      unmask = "";
       prepareMask();
     };
 
@@ -111,21 +111,21 @@
 
   ko.validation.rules["isValidEmail"] = {
     validator: function (val, params) {
-      params[0] = 'El formato del email no es válido. - Su email tiene caracteres especiales no permitidos para la registración en nuestra página.'
+      params[0] = "El formato del email no es válido. - Su email tiene caracteres especiales no permitidos para la registración en nuestra página.";
       var re = /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/i;
       if (re.test(val) != true) {
         var re2 = /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9\u00E1\u00E9\u00ED\u00F3\u00FA\u00F1\u00FC]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/i;
         if (re2.test(val) == true) {
-          params[0] = params[0].split('-')[1];
+          params[0] = params[0].split("-")[1];
           return false;
         } else {
-          params[0] = params[0].split('-')[0];
+          params[0] = params[0].split("-")[0];
           return false;
         }
       } else {
         return true;
       }
     },
-    message: '{0}'
+    message: "{0}",
   };
 })();
